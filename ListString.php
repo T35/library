d0f15de3ece09b7144706e387f96da0b7c9de49f
T35 <esmelnikov@gmailcom>
2022-01-05 19:10:14 +0300

    2022.01.05 #2

--- ListString.php
+++ ListString.php
@@ -3,17 +3,10 @@
 namespace t35\Library;
 
 class ListString extends ListSimple {
-    /**
-     * Реализация $box с измененным PHPDoc.
-     *
-     * @var array<int, string>
-     */
-    protected array $box = [];
-
     public function offsetSet(mixed $offset, mixed $value): void {
-        if (!is_string($value)) {
+        if (!($value instanceof StringBase)) {
             throw new \InvalidArgumentException(
-                'Элемент "' . static::class . '" должен быть класса строкой(string). Передан "' . get_debug_type($value) . '"'
+                'Элемент "' . static::class . '" должен быть класса строкой "' . StringBase::class . '". Передан "' . get_debug_type($value) . '"'
             );
         }
 
