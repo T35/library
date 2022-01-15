cc985fea1d568e5225db14c8d57ac3f6b7f71116
T35 <esmelnikov@gmailcom>
2022-01-15 03:01:57 +0300

    2021.01.15 #3

--- StringBase.php
+++ StringBase.php
@@ -6,16 +6,59 @@ use JetBrains\PhpStorm\Pure;
 use TypeError;
 
 class StringBase extends BaseClass implements IJSONSerializable {
+    /** @var string Базовая строка */
     protected string $string;
 
     /**
+     * @var StringBase Кодировка строки до применения конвертации, если та была произведена.
+     * @see StringBase::ConverseToUTF8()
+     */
+    protected StringBase $InitEncoding;
+
+    /**
+     * Возвращает кодировку базовой строки до применения конвертации.
+     * Если конвертация не применялась, вернет "false".
+     *
+     * @return StringBase|false
+     */
+    public function InitEncoding(): StringBase|false {
+        return $this->InitEncoding ?? false;
+    }
+
+    /**
+     * @var array Массив, содержащий список возможных кодировок для ТОЧНОГО(strict) определения кодировки при применении конвертации.
+     * @see StringBase::ConverseToUTF8()
+     */
+    protected static array $encodings = [
+        'UTF-8',
+        'Windows-1251',
+        'ISO-8859-1',
+        'ASCII'
+    ];
+
+    /**
+     * Переопределение списка кодировок для определения кодировки базовой строки в процессе конвертации в UTF-8.
+     *
+     * @param ListString $listEncodings
+     * @return void
+     *@see StringBase::ConverseToUTF8()
+     */
+    public static function SetEncodings(ListString $listEncodings): void {
+        self::$encodings = $listEncodings->toArray();
+    }
+
+    /**
      * @param mixed $value
+     * @param bool $converseToUTF8 Если "true", входная строка будет приведена к UTF-8, если это возможно.
      * @throws stdException
      */
     public function __construct(
-        mixed $value = ''
+        mixed $value = '',
+        bool  $converseToUTF8 = false
     ) {
         $this->Set($value);
+        if ($converseToUTF8)
+            $this->ConverseToUTF8();
     }
 
     /**
@@ -23,13 +66,13 @@ class StringBase extends BaseClass implements IJSONSerializable {
      * Например, параметры типа string вначале всех методов, для удобства, нужно преобразовать в объект StringBase static класса.
      *
      * @param mixed $value
-     * @param EStringFormat $format
+     * @param bool $converseToUTF8
      * @return void
      * @throws stdException
      */
-    public static function Converse(mixed &$value, EStringFormat $format = EStringFormat::None): void {
+    public static function Converse(mixed &$value, bool $converseToUTF8 = false): void {
         if (!($value instanceof StringBase)) {
-            $value = new static($value, $format);
+            $value = new static($value, $converseToUTF8);
         }
     }
 
@@ -123,10 +166,33 @@ class StringBase extends BaseClass implements IJSONSerializable {
     /**
      * Реализация интерфейса.
      *
-     * @see IJSONSerializable
      * @return string
+     * @see IJSONSerializable
      */
     #[Pure] public function JSONSerialize(): string {
         return $this->GetString();
+    }
+
+    /**
+     * Приведение исходной строки к UTF-8.
+     *
+     * @return void
+     * @throws stdException
+     */
+    public function ConverseToUTF8(): void {
+        if ($encoding = mb_detect_encoding($this->string, static::$encodings, true)) {
+            $this->InitEncoding = new StringBase($encoding);
+
+            $this->string = mb_convert_encoding($this->string, 'UTF-8', $this->InitEncoding());
+        }
+        else {
+            throw new stdException(
+                'Не удалось определить кодировку строки',
+                [
+                    'encodings' => static::$encodings,
+                    'string' => $this->string
+                ]
+            );
+        }
     }
 }
